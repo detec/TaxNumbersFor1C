00d85052bb8f2cefb99bcb87a63039b01b4b34b2
detec <dlit@yandex.ru>
2015-12-29 22:07:45 +0200

    Problem with carriage returm

---
+++ target/test-classes/com/malbi/taxnumbers/db/MockConnectionManager.java
@@ -0,0 +1,25 @@
+package com.malbi.taxnumbers.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+// Let's try http://www.javaranch.com/journal/2003/12/UnitTestingDatabaseCode.html
+
+public class MockConnectionManager implements IConnectionManager {
+
+	@Override
+	public Connection getDBConnection() throws SQLException {
+		Connection con = null;
+		try {
+			Class driverClass = Class.forName("org.h2.Driver");
+			con = DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "");
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return con;
+	}
+
+}
---
+++ target/test-classes/com/malbi/taxnumbers/util/AcceptedDateFormat.java
@@ -0,0 +1,9 @@
+package com.malbi.taxnumbers.util;
+
+public class AcceptedDateFormat {
+
+	public static final String TimeStampFormat = "yyyy-MM-dd HH:mm:ss.S";
+
+	public static final String OracleDateFormat = "dd.MM.yyyy";
+
+}
