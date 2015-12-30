cc9ddba35c6574a90a6364711413d76eca57235d
duplyk.a <duplyk.a@developer.malbi.lan>
2015-12-30 09:33:11 +0200

    Seem to have fixed carriage return problem

--- target/test-classes/com/malbi/taxnumbers/util/AcceptedDateFormat.java
+++
@@ -1,9 +0,0 @@
-package com.malbi.taxnumbers.util;
-
-public class AcceptedDateFormat {
-
-	public static final String TimeStampFormat = "yyyy-MM-dd HH:mm:ss.S";
-
-	public static final String OracleDateFormat = "dd.MM.yyyy";
-
-}
